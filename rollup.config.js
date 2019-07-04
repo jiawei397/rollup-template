68f3702f4dcc1e2f7634044a30b2e5e73f13f253
jw397 <jw397@126.com>
2019-07-04 15:32:29 +0800

    feat: rollup使用模板

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
 import {uglify} from 'rollup-plugin-uglify'; //压缩bundle文件
 
 const isProduction = process.env.NODE_ENV === 'production';
-const input = 'src/menus/main.js';
+const input = 'src/index.js';
 
 export default {
   input: input,
   output: {
-    file: `lib/menuTools.${isProduction ? 'min.' : ''}js`,
+    file: `lib/XX.${isProduction ? 'min.' : ''}js`,
     format: 'umd',
-    name: 'menuTools'
+    name: 'XX'
   },
   plugins: [
     (isProduction && uglify())
